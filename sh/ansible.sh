5c1f018add984aae84b3d6a62acf50b0b744a7cd
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-05-08 09:55:21 +0900

    *

--- sh/ansible.sh
+++ sh/ansible.sh
@@ -4,5 +4,12 @@ sudo pacman -S git python2 python2-pip
 sudo pip2 install --upgrade pip
 sudo pip2 install ansible
 
-mkdir ~/.ghq/github.com/suzuki-shunsuke
-git clone https://github.com/suzuki-shunsuke/ansible.arch ~/.ghq/github.com/suzuki-shunsuke/ansible.arch
+if [ ! -d ~/.ghq/github.com/suzuki-shunsuke ]
+then
+    mkdir ~/.ghq/github.com/suzuki-shunsuke
+fi
+
+if [ ! -d ~/.ghq/github.com/suzuki-shunsuke/ansible.arch ]
+then
+    git clone https://github.com/suzuki-shunsuke/ansible.arch ~/.ghq/github.com/suzuki-shunsuke/ansible.arch
+fi
